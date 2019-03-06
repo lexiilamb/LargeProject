ccc3174db2147c0a03a62dfaa55abd78909a5e2c
Luke Hazelton <tomluke600@gmail.com>
2019-03-06 16:17:10 -0500

    Updated port value
    Updated so that the server will be connected (hopefully) through Heroku's port when deploying through there, and will only resort to custom port 4000 when dev testing (local deployment)

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; // "process.env.PORT" is Heroku's port if we're deploying there, then 4000 is a custom chosen port for dev testing
 let Todo = require('./todo.model');
 app.use(cors());
 app.use(bodyParser.json());
