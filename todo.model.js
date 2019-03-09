e0d67a16ebf4bf2230ff4cf1ceef5971bb4253d5
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-03-08 23:37:22 -0500

    pt.4 again
    missed antoher schema edit

--- todo.model.js
+++ todo.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let Todo = new Schema({
-    todo_description: {
+    description: {
         type: String
     },
     amount: {
