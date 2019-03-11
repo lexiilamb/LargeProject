1d0b6c7a73568485c646558e3ad24941538a600e
Lexii <37764077+lexiilamb@users.noreply.github.com>
2019-03-11 00:14:40 -0400

    plz

--- client/src/components/editExpense.js
+++ client/src/components/editExpense.js
@@ -87,7 +87,8 @@ export default class EditExpense extends Component {
         this.props.history.push('/');
     }
 	
-	deleteExpense() {		
+	deleteExpense(e) {	
+		e.preventDefault();
         axios.delete('/expenses/delete/'+this.props.match.params.id)
             .then(res => console.log(res.data));
         
