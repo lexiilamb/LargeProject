9e77f06df8f437e9d1baff6b0a349ebd0c5516a4
Lexii <37764077+lexiilamb@users.noreply.github.com>
2019-03-12 14:50:15 -0400

    month and day dropdown/select working

--- client/src/components/createExpense.js
+++ client/src/components/createExpense.js
@@ -2,11 +2,55 @@ import React, { Component } from 'react';
 import Select from 'react-select';
 import axios from 'axios';
 
-const options = [
+const optionsMonth = [
   { value: 'Jan', label: 'Jan' },
   { value: 'Feb', label: 'Feb' },
+  { value: 'March', label: 'March' },
+  { value: 'April', label: 'April' },
+  { value: 'May', label: 'May' },
+  { value: 'June', label: 'June' },
+  { value: 'July', label: 'July' },
+  { value: 'Aug', label: 'Aug' },
+  { value: 'Sep', label: 'Sep' },
+  { value: 'Oct', label: 'Oct' },
+  { value: 'Nov', label: 'Nov' },
   { value: 'Dec', label: 'Dec' }
 ];
+
+const optionsDay = [
+  { value: 1, label: 1 },
+  { value: 2, label: 2 },
+  { value: 3, label: 3 },
+  { value: 4, label: 4 },
+  { value: 5, label: 5 },
+  { value: 6, label: 6 },
+  { value: 7, label: 7 },
+  { value: 8, label: 8 },
+  { value: 9, label: 9 },
+  { value: 10, label: 10 },
+  { value: 11, label: 11 },
+  { value: 12, label: 12 },
+  { value: 13, label: 13 },
+  { value: 14, label: 14 },
+  { value: 15, label: 15 },
+  { value: 16, label: 16 },
+  { value: 17, label: 17 },
+  { value: 18, label: 18 },
+  { value: 19, label: 19 },
+  { value: 20, label: 20 },
+  { value: 21, label: 21 },
+  { value: 22, label: 22 },
+  { value: 23, label: 23 },
+  { value: 24, label: 24 },
+  { value: 25, label: 25 },
+  { value: 26, label: 26 },
+  { value: 27, label: 27 },
+  { value: 28, label: 28 },
+  { value: 29, label: 29 },
+  { value: 30, label: 30 },
+  { value: 31, label: 31 }
+];
+
 
 export default class CreateExpense extends Component {
 
@@ -24,7 +68,7 @@ export default class CreateExpense extends Component {
             description: '',
             amount: '',
             selectedMonth: '',
-            day: '',
+            selectedDay: '',
             year: ''
         }
     }
@@ -45,12 +89,11 @@ export default class CreateExpense extends Component {
         this.setState({
             month: selectedMonth.value
         });
-		console.log(`Option selected:`, selectedMonth.value);
     }
 	
-	onChangeDay(e) {
+	onChangeDay(selectedDay) {
         this.setState({
-            day: e.target.value
+            day: selectedDay.value
         });
     }
 	
@@ -95,6 +138,7 @@ export default class CreateExpense extends Component {
 
     render() {
 		const { selectedMonth } = this.state;
+		const { selectedDay } = this.state;
 		
         return (
             <div style={{marginTop: 10}}>
@@ -111,31 +155,32 @@ export default class CreateExpense extends Component {
                     <div className="form-group">
                         <label>Amount: </label>
                         <input 
-                                type="text" 
-                                className="form-control"
-                                value={this.state.amount}
-                                onChange={this.onChangeAmount}
-                                />
+							type="text" 
+							className="form-control"
+							value={this.state.amount}
+							onChange={this.onChangeAmount}
+							/>
                     </div>
-				
 					<div className="form-group">
 					  <label>Month: </label>
 					  <Select
+						name="Month"
+						placeholder={this.state.month}
 						value={selectedMonth}
+						options={optionsMonth}
 						onChange={this.onChangeMonth}
-						options={options}
+					  />
+					</div>					
+					<div className="form-group">
+					  <label>Day: </label>
+					  <Select
+						name="Day"
+						placeholder={this.state.day}
+						value={selectedDay}
+						options={optionsDay}
+						onChange={this.onChangeDay}
 					  />
 					</div>
-					
-	
-					<div className="form-group"> 
-                        <label>Day: </label>
-                        <input  type="text"
-                                className="form-control"
-                                value={this.state.day}
-                                onChange={this.onChangeDay}
-                                />
-                    </div>
 					<div className="form-group"> 
                         <label>Year: </label>
                         <input  type="text"
