10cc6f24e64d4cb7ca0ee140b348b9222862a06e
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-03-13 02:06:51 -0400

    .

--- client/src/components/monthly.js
+++ client/src/components/monthly.js
@@ -37,7 +37,7 @@ export default class TodosList extends Component {
 	
 	componentDidMount() {		
         axios.get('/expenses/month/'+'Jan')
-            ..then(response => {
+            .then(response => {
 				temp = response.data;
 				temp = sortBy(temp, ['description', 'amount']);
 				sum = sumBy(temp, 'amount');
